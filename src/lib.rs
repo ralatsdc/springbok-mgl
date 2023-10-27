d70da73e82d25d912ebb7d1a78a9bdc5fabc4e72
Raymond LeClair <raymond.leclair@springbok.io>
2023-10-27 16:00:46 -0400

    Construct search URL

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,17 @@
-use clap::{Parser, ValueEnum};
+use clap::Parser;
+use indexmap::IndexMap;
+use log::debug;
+use scraper::{Html, Selector};
+use url::Url;
 
 // See:
 // - https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_0/index.html#
-// - https://docs.rs/clap/latest/clap/_derive/_tutorial/chapter_3/index.html
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum Branch {
-    House,
-    Senate,
-}
-
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum DocumentType {
-    Amendment,
-    Bill,
-    Communication,
-    Order,
-    Report,
-    Resolve,
-}
+// - https://docs.rs/clap/latest/clap/struct.Arg.html#method.default_missing_value
+// - https://docs.rs/reqwest/latest/reqwest/blocking/index.html
+// - https://docs.rs/indexmap/latest/indexmap/index.html
+// - https://docs.rs/scraper/0.17.1/scraper/#
+// - https://docs.rs/url/latest/url/#
+// - https://docs.rs/url/latest/url/struct.Url.html#
 
 /// Produce strikethrough and underline markup for a bill before the Massachusetts legislature
 #[derive(Parser)]
@@ -29,26 +22,96 @@ pub struct Cli {
     pub search_term: Option<String>,
 
     /// Identify legislation from this legislative session
-    #[arg(short = 'c', long)]
-    general_court: Option<String>,
+    #[arg(short = 'c', long, default_value = "193rd", num_args = 0..=1, default_missing_value = "MISSING")]
+    pub general_court: Option<String>,
 
     /// Include legislation in this branch of the legislature
-    #[arg(short = 'b', long, value_enum)]
-    branch: Option<Branch>,
+    #[arg(short = 'b', long, num_args = 0..=1, default_missing_value = "MISSING")]
+    pub branch: Option<String>,
 
     /// Include legislation sponsored by this legislator
-    #[arg(short = 'l', long)]
-    sponsor_legislator: Option<String>,
+    #[arg(short = 'l', long, num_args = 0..=1, default_missing_value = "MISSING")]
+    pub sponsor_legislator: Option<String>,
 
     /// Include legislation sponsored by this legislative committee
-    #[arg(short = 'm', long)]
-    sponsor_committee: Option<String>,
+    #[arg(short = 'm', long, num_args = 0..=1, default_missing_value = "MISSING")]
+    pub sponsor_committee: Option<String>,
 
     /// Include legislation sponsored by a governor, or state organization
-    #[arg(short = 'o', long)]
-    sponsor_other: Option<String>,
+    #[arg(short = 'o', long, num_args = 0..=1, default_missing_value = "MISSING")]
+    pub sponsor_other: Option<String>,
 
     /// Identify legislation of this document type
-    #[arg(short = 'd', long, value_enum)]
-    document_type: Option<DocumentType>,
+    #[arg(short = 'd', long, num_args = 0..=1, default_missing_value = "MISSING")]
+    pub document_type: Option<String>,
+}
+
+pub fn create_refiner_map(refiner_map: &mut IndexMap<String, IndexMap<String, Entry>>) {
+    let body = reqwest::blocking::get("https://malegislature.gov/Bills/Search?SearchTerms=&Page=1")
+        .unwrap()
+        .text()
+        .unwrap();
+    let document = Html::parse_document(body.as_str());
+
+    let refiner_selector = Selector::parse("div#refiners fieldset").unwrap();
+    // Note that removing the "top" class will collect all hidden groups, some of which will be duplicates
+    let group_selector = Selector::parse("div.refinerGroup.top").unwrap();
+    let legend_selector = Selector::parse("legend").unwrap();
+    let label_selector = Selector::parse("label").unwrap();
+    let input_selector = Selector::parse("input").unwrap();
+    for refiner_element in document.select(&refiner_selector) {
+        let legend_element = refiner_element.select(&legend_selector).next().unwrap();
+        let legend_text = legend_element.text().collect::<Vec<_>>()[0].trim();
+        debug!("\nLegend text: {:?}", legend_text);
+
+        let mut group_map = IndexMap::new();
+
+        let group_element = refiner_element.select(&group_selector).next().unwrap();
+        for label_element in group_element.select(&label_selector) {
+            let label_text = label_element.text().collect::<Vec<_>>()[1].trim();
+            debug!("\nLabel text: {:?}", label_text);
+
+            let entry_text: Vec<&str> = label_text.split(' ').collect();
+            debug!("Entry: {:}", entry_text[0]);
+
+            let input_element = label_element.select(&input_selector).next().unwrap();
+            let entry_token = input_element.value().attr("data-refinertoken").unwrap();
+            debug!("Entry token: {}", entry_token);
+
+            group_map.insert(
+                String::from(entry_text[0]),
+                Entry {
+                    label_text: String::from(label_text),
+                    entry_token: String::from(entry_token),
+                },
+            );
+        }
+        refiner_map.insert(String::from(legend_text), group_map);
+    }
+}
+
+pub struct Entry {
+    pub label_text: String,
+    pub entry_token: String,
+}
+
+pub fn list_entries_or_append_query_pair(
+    argument: &Option<String>,
+    map: &IndexMap<String, Entry>,
+    field: &mut String,
+    search_url: &mut Url,
+) {
+    match argument {
+        Some(value) if value == &String::from("MISSING") => {
+            for (key, entry) in map.iter() {
+                println!(r#"Use "{}" for "{}""#, key, entry.label_text);
+            }
+        }
+        Some(value) => {
+            search_url
+                .query_pairs_mut()
+                .append_pair(field, map.get(value).unwrap().entry_token.as_str());
+        }
+        None => (),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,81 @@
 use clap::Parser;
 use indexmap::IndexMap;
-use log::{info, warn};
-use scraper::{Html, Selector};
+use log::info;
 use url::Url;
 
-// See:
-// - https://docs.rs/reqwest/latest/reqwest/blocking/index.html
-// - https://docs.rs/indexmap/latest/indexmap/index.html
-// - https://docs.rs/scraper/0.17.1/scraper/#
-// - https://docs.rs/url/latest/url/#
-// - https://docs.rs/url/latest/url/struct.Url.html#
-
-use springbok_mgl::Cli;
-
-struct Entry {
-    label_text: String,
-    entry_token: String,
-}
+use springbok_mgl::{create_refiner_map, list_entries_or_append_query_pair, Cli};
 
 fn main() {
     env_logger::init();
-    info!("starting up");
-    warn!("oops, nothing implemented!");
 
-    // Demonstrate getting and parsing a page to get needed search tokens
+    // Get default search page, parse, and create refiner map
 
-    let body = reqwest::blocking::get("https://malegislature.gov/Bills/Search?SearchTerms=&Page=1")
-        .unwrap()
-        .text()
-        .unwrap();
-    let document = Html::parse_document(body.as_str());
-
+    info!("Creating refiner map");
     let mut refiner_map = IndexMap::new();
+    create_refiner_map(&mut refiner_map);
 
-    let refiner_selector = Selector::parse("div#refiners fieldset").unwrap();
-    // Note that removing the "top" class will collect all hidden groups, some of which will be duplicates
-    let group_selector = Selector::parse("div.refinerGroup.top").unwrap();
-    let legend_selector = Selector::parse("legend").unwrap();
-    let label_selector = Selector::parse("label").unwrap();
-    let input_selector = Selector::parse("input").unwrap();
-    for refiner_element in document.select(&refiner_selector) {
-        let legend_element = refiner_element.select(&legend_selector).next().unwrap();
-        let legend_text = legend_element.text().collect::<Vec<_>>()[0].trim();
-        println!("\nLegend text: {:?}", legend_text);
+    // Parsing command line arguments and construct search URL
 
-        let mut group_map = IndexMap::new();
-
-        let group_element = refiner_element.select(&group_selector).next().unwrap();
-        for label_element in group_element.select(&label_selector) {
-            let label_text = label_element.text().collect::<Vec<_>>()[1].trim();
-            println!("Label text: {:?}", label_text);
-
-            let entry_text: Vec<&str> = label_text.split(' ').collect();
-            println!("Entry: {:}", entry_text[0]);
-
-            let input_element = label_element.select(&input_selector).next().unwrap();
-            let entry_token = input_element.value().attr("data-refinertoken").unwrap();
-            println!("Entry token: {}", entry_token);
-
-            group_map.insert(
-                String::from(entry_text[0]),
-                Entry {
-                    label_text: String::from(label_text),
-                    entry_token: String::from(entry_token),
-                },
-            );
-        }
-
-        refiner_map.insert(String::from(legend_text), group_map);
-    }
-
-    // Demonstrate parsing and handling of command line arguments
-
+    info!("Constructing search URL");
     let cli = Cli::parse();
-
     let mut search_url = Url::parse("https://malegislature.gov/Bills/Search").unwrap();
 
-    // https://malegislature.gov/Bills/Search?SearchTerms=mbta
-    if let Some(search_term) = cli.search_term {
-        println!("Value for search term: {search_term}");
-        search_url.set_query(Some(format!("SearchTerms={search_term}").as_str()));
+    // https://malegislature.gov/Bills/Search?SearchTerms=&Page=1
+    // https://malegislature.gov/Bills/Search?SearchTerms=mbta&Page=1
+    match cli.search_term {
+        None => search_url.set_query(Some("SearchTerms=&Page=1")),
+        Some(search_term) => {
+            search_url.set_query(Some(format!("SearchTerms={search_term}&Page=1").as_str()))
+        }
     }
 
-    println!("Value for search URL: {search_url}");
+    // https://malegislature.gov/Bills/Search
+    // https://malegislature.gov/Bills/Search?SearchTerms=&Page=1&Refinements%5Blawsgeneralcourt%5D=3139326e64202832303231202d203230323229
+    list_entries_or_append_query_pair(
+        &cli.general_court,
+        refiner_map.get("General Court").unwrap(),
+        &mut String::from("Refinements[lawsgeneralcourt]"),
+        &mut search_url,
+    );
+
+    // https://malegislature.gov/Bills/Search?SearchTerms=&Page=1&Refinements%5Blawsbranchname%5D=486f757365
+    list_entries_or_append_query_pair(
+        &cli.branch,
+        refiner_map.get("Branch").unwrap(),
+        &mut String::from("Refinements[lawsbranchname]"),
+        &mut search_url,
+    );
+
+    // https://malegislature.gov/Bills/Search?SearchTerms=&Page=1&Refinements%5Blawsuserprimarysponsorname%5D=4172636965726f2c204a616d6573
+    list_entries_or_append_query_pair(
+        &cli.sponsor_legislator,
+        refiner_map.get("Sponsor — Legislator").unwrap(),
+        &mut String::from("Refinements[lawsuserprimarysponsorname]"),
+        &mut search_url,
+    );
+
+    // https://malegislature.gov/Bills/Search?SearchTerms=&Page=1&Refinements%5Blawscommitteeprimarysponsorname%5D=3139326e64204a52756c6573
+    list_entries_or_append_query_pair(
+        &cli.sponsor_committee,
+        refiner_map.get("Sponsor — Committee").unwrap(),
+        &mut String::from("Refinements[lawscommitteeprimarysponsorname]"),
+        &mut search_url,
+    );
+
+    // https://malegislature.gov/Bills/Search?SearchTerms=&Page=1&Refinements%5Blawsotherprimarysponsorname%5D=41756469746f72206f662074686520436f6d6d6f6e7765616c7468
+    list_entries_or_append_query_pair(
+        &cli.sponsor_other,
+        refiner_map.get("Sponsor — Other").unwrap(),
+        &mut String::from("Refinements[lawsotherprimarysponsorname]"),
+        &mut search_url,
+    );
+
+    // https://malegislature.gov/Bills/Search?SearchTerms=&Page=1&Refinements%5Blawsfilingtype%5D=416d656e646d6%%6e74
+    list_entries_or_append_query_pair(
+        &cli.document_type,
+        refiner_map.get("Document Type").unwrap(),
+        &mut String::from("Refinements[lawsfilingtype]"),
+        &mut search_url,
+    );
+    info!("Value for search URL: {search_url}");
 }
