64f05eac53282190544e0290626cea7a1084e69b
Raymond LeClair <raymond.leclair@springbok.io>
2023-11-08 10:15:01 -0500

    Add output of bill text

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use log::info;
 use url::Url;
 
 use springbok_mgl::{
-    create_refiner_map, download_bill_text, get_and_print_search_results,
+    create_refiner_map, get_and_print_bill_text_nodes, get_and_print_search_results,
     print_entries_or_append_query_pair, Cli,
 };
 
@@ -109,12 +109,12 @@ fn main() {
         search_results_map = get_and_print_search_results(&search_url);
     }
 
-    // Download the bill text when searching with bill number
+    // Get, and print the bill text when searching with bill number
     if cli.download {
         if let Some(&ref search_entry) = search_results_map.get(search_term.as_str()).as_deref() {
             let bill_url = &search_entry.bill_url;
             info!("Value for bill URL: {bill_url}");
-            download_bill_text(bill_url);
+            get_and_print_bill_text_nodes(bill_url, cli.output_filename);
         } else {
             info!("Search term is not a bill number")
         }
